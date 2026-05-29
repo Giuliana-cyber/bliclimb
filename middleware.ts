54bd7004d87df9165229b9d61c7cedd8a4d96e88
Giuliana-cyber <Giuliana@belaypartners.org>
2026-05-29 16:16:26 -0600

    fix: include clerk proxy matcher

--- middleware.ts
+++ middleware.ts
@@ -42,6 +42,7 @@ export function middleware(request: NextRequest, event: NextFetchEvent) {
 export const config = {
   matcher: [
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    '/(api)(.*)'
+    '/(api|trpc)(.*)',
+    '/__clerk/(.*)'
   ]
 };
